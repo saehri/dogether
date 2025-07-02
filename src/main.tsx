ba65b98d153bcbb02d754f103a8ca9cb8d90c46f
saehri <bahreesaepul1@gmail.com>
2025-07-02 20:21:47 +0700

    refactor: Remove Layout and ProtectedRoute components; adjust SWRConfig and PublicRoutes for improved structure

--- src/main.tsx
+++ src/main.tsx
@@ -9,17 +9,17 @@ import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<SWRConfig
-			value={{
-				refreshInterval: 3000,
-				fetcher: (resource, init) =>
-					fetch(resource, init).then((res) => res.json()),
-			}}
-		>
-			<ThemeProvider>
+		<ThemeProvider>
+			<SWRConfig
+				value={{
+					refreshInterval: 3000,
+					fetcher: (resource, init) =>
+						fetch(resource, init).then((res) => res.json()),
+				}}
+			>
 				<App />
-			</ThemeProvider>
-		</SWRConfig>
+			</SWRConfig>
+		</ThemeProvider>
 	</StrictMode>
 );
 
--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -1,9 +1,11 @@
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
 
 const PublicRoutes = () => {
 	const isAuthenticated = useAuth();
-	return !isAuthenticated ? <Outlet /> : <Navigate to="/feed" />;
+
+	if (!isAuthenticated) return <Navigate to="/login" />;
+	return <Navigate to="/feed" />;
 };
 
 export default PublicRoutes;
